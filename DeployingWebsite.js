3113b28401438fee313203ea07b0be743460a46f
Shobyy <shoaib.kxoft@gmail.com>
2018-12-24 18:58:59 +0500

    Port Change

--- DeployingWebsite.js
+++ DeployingWebsite.js
@@ -3,7 +3,7 @@ const hbs = require('hbs');
 const express = require('express')
 var app = express();
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 4000;
 
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -13,12 +13,12 @@ app.use(express.static(__dirname + '/Public'));
 
 /*Using ExpressMiddleWare Tutorial# 6*/
 
-app.use( (req, res, next) => {
+/*app.use( (req, res, next) => {
     var now = new Date().toString();
 
     console.log(`${now}: ${req.method} ${req.url}`);
     next();
-});
+});*/
 
 /*app.use((req, res, next) => {
 res.render('maintenance.hbs')
